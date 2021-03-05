a1226adb96b9fd26219948f51dc9733574cd3ee6
maggot_code <wowhuanhuan233@163.com>
2021-03-05 22:17:32 +0800

    fix：remove tag mill
    1.移除项目中的 tag mill 模块

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@
  * @Author: maggot-code
  * @Date: 2021-03-03 16:50:39
  * @LastEditors: maggot-code
- * @LastEditTime: 2021-03-04 18:21:11
+ * @LastEditTime: 2021-03-05 15:08:42
  * @Description: mg form component inlet
  */
 import '../plugins/element';
-import '../packages/mg-form/tag-mill';
 import MgForm from '../packages/mg-form';
 import MgInput from '../packages/mg-input';
 import MgSelect from '../packages/mg-select';
-import Tagmill from '../packages/mg-form/tag-mill';
 
 const components = [
     MgForm,
@@ -22,8 +20,6 @@ const install = function (Vue, opts = {}) {
     components.forEach(component => {
         Vue.component(component.name, component);
     });
-
-    Vue.prototype.$Tagmill = new Tagmill();
 };
 
 /* istanbul ignore if */
